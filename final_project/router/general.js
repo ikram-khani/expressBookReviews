bb81209fd3482b39cf6c95ce3e3a1750d34ed031
ikramkhan <ikr302000@gmail.com>
2025-02-25 16:53:38 +0500

    task3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,10 +30,25 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author; // Extract author from request parameters
+  let booksByAuthor = [];
+
+  // Iterate through the books object
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].author === author) {
+      booksByAuthor.push({ isbn, ...books[isbn] });
+    }
+  });
+
+  // Check if any books were found
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
